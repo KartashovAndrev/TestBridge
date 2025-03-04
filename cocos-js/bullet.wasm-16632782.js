bb30c3772381629757c9c1002b78a2b85c41b0fe
unknown <kartashow1998@gmail.com>
2025-03-04 11:30:54 +0300

    fixed adaptive layout issues, lever behavior, board block physics

---
+++ cocos-js/bullet.wasm-16632782.js
@@ -0,0 +1,2 @@
+System.register([],(function(e,t){"use strict";return{execute:function(){e("default",new URL("assets/bullet.wasm-9d17e9bf.wasm",t.meta.url).href)}}}));
+//# sourceMappingURL=bullet.wasm-16632782.js.map
